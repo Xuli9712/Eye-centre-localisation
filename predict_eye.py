051e705a397a1debfba39ecf2c91951710743034
Wang <80760723+Xuli9712@users.noreply.github.com>
2023-09-14 21:30:13 +0100

    Add files via upload

--- predict_eye.py
+++ predict_eye.py
@@ -1,7 +1,7 @@
 import cv2
 import torch
 import numpy as np
-import sys, json
+import sys
 import os
 import torch.backends.cudnn as cudnn
 from PIL import Image
@@ -10,69 +10,50 @@ from model.Unet import UNet
 from model.Unetpp import NestedUNet
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
-from tools.heatmap_trans import *
-from tqdm import tqdm 
+from tools.heatmap_trans import extract_keypoints_from_heatmap, extract_keypoints_from_heatmap_to_ori_size
 
 def predict_eye_center(model, img_path, weight_path, img_size):
+    # eval mode
+    model.eval()
     # Load and preprocess the image
     image = Image.open(img_path)
-    img_copy1 = image.copy()
-    img_copy2 = image.copy()
-    W, H = image.size
+    img_copy = image.copy()
+    W, H = img_copy.size
     #img_copy = img_copy.resize(img_size)
-    W = torch.tensor(W).cuda()
-    H = torch.tensor(H).cuda()
     
     image = preprocess_image(image, img_size)
 
+    model = load_model(model, weight_path)
+
+    model = model.cuda()
     image = image.cuda()
 
     with torch.no_grad():
         output = model(image)
 
-    #output = output.squeeze().cpu().numpy()
+    output = output.squeeze().cpu().numpy()
     
     num_keypoints = output.shape[0]
     heatmap_size = output.shape[1:]
 
     #keypoints = extract_keypoints_from_heatmap(output)
-    #keypoints = extract_keypoints_from_heatmap_to_ori_size(output, W, H)
-    keypoints = heatmap2coord(output, W, H).squeeze().cpu().numpy()
-    #print(keypoints)
+    keypoints = extract_keypoints_from_heatmap_to_ori_size(output, W, H)
+    print(keypoints)
 
-    for i in range(3):
-        img_copy2 = cv2.circle(np.array(img_copy2), tuple(map(int, keypoints[i])), 1, (0, 0, 255), -1)
-    
-    output = output.squeeze().cpu().numpy()
-    
-     # 显示左侧的单张图像, 点已经绘制上去了
-    plt.subplot(1, 5 , 1)
-    plt.imshow(img_copy1)
-    plt.title(f"Input")
-    '''
-    # 显示右侧的热力图
+    for i in range(num_keypoints):
+        img_copy = cv2.circle(np.array(img_copy), tuple(map(int, keypoints[i])), 1, (0, 0, 255), -1)
+
+    plt.subplot(1, 5, 1)
+    plt.imshow(img_copy)
+
     for i in range(3):
         plt.subplot(1, 5, i+3)
         plt.imshow(output[i], cmap='hot')
         plt.title(f"Target {i+1}")
-    '''
-    
-    for i in range(3):
-        plt.subplot(1, 5, i+2)
-        plt.imshow(output[i], cmap='hot')
-        plt.title(f"Output{i+1}")
-    
-    plt.subplot(1, 5 , 5)
-    plt.imshow(img_copy2)
-    plt.title(f"Result")
-    
+
     plt.tight_layout()  
     plt.show()
-
-    return img_copy2, keypoints
-
-
-
+    return img_copy, keypoints
 
 def load_model(model, weight_path):
     if weight_path is None:
@@ -105,9 +86,9 @@ def predict_eye_center_module(processed_image, image_W, image_H):
     image = image.cuda()
 
     with torch.no_grad():
-        outputs = model(processed_image)
+        output = model(processed_image)
 
-    output = outputs[3].squeeze().cpu().numpy()
+    output = output.squeeze().cpu().numpy()
     
     num_keypoints = output.shape[0]
     heatmap_size = output.shape[1:]
@@ -119,22 +100,14 @@ def predict_eye_center_module(processed_image, image_W, image_H):
     for i in range(num_keypoints):
         img_copy = cv2.circle(np.array(img_copy), tuple(map(int, keypoints[i])), 1, (0, 0, 255), -1)
 
-    # 显示左侧的单张图像, 点已经绘制上去了
-    plt.subplot(1, 13 , 1)
+    plt.subplot(1, 5, 1)
     plt.imshow(img_copy)
-    '''
+
     # 显示右侧的热力图
     for i in range(3):
         plt.subplot(1, 5, i+3)
         plt.imshow(output[i], cmap='hot')
         plt.title(f"Target {i+1}")
-    '''
-    
-    for j, output in enumerate(outputs):
-        for i in range(3):
-            plt.subplot(1, 5, i+2+3*j)
-            plt.imshow(output[j][i], cmap='hot')
-            plt.title(f"Target{j+1} {i+1}")
 
     plt.tight_layout()  
     plt.show()
@@ -156,21 +129,11 @@ if __name__ == '__main__':
     #image_path = r'F:\gi4e_database\Leyes\053_07.png'
     root_path = r'D:\gi4e_database\blend'
     img_list = os.listdir(root_path)
-    weight_path = r'Best\best.pth'
+    weight_path = r'C:\Users\Xuli\Desktop\hrnet_gi4e\results\0719_0038\best.pth'
     result_path = os.path.join(root_path, 'results')
-    if not os.path.exists(result_path):
-        os.makedirs(result_path)
-    #model = UNet()
-    model = NestedUNet(False)
-    # eval mode
-    model.eval()
-    model = load_model(model, weight_path)
-    model = model.cuda()
-    for img_name in tqdm(img_list):
+    model = UNet()
+    #model = NestedUNet(False)
+    for img_name in img_list:
         image_path = os.path.join(root_path, img_name)
-        result_image, keypoints = predict_eye_center(model, image_path, weight_path, img_size=(64, 64))
-        result_image_bgr = cv2.cvtColor(result_image, cv2.COLOR_RGB2BGR)
-        result_image_path = os.path.join(result_path, img_name)
-        cv2.imwrite(result_image_path, result_image_bgr)
-
+        result_image, keypoints = predict_eye_center(model, image_path, weight_path, img_size=(32, 32))
         
---
+++ train_eye_max.py
@@ -0,0 +1,397 @@
+import os, argparse,json, sys
+import numpy as np
+from tqdm import tqdm
+from datetime import datetime
+from tools.heatmap_trans import *
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import csv
+import json
+from model.Unet import UNet
+from model.Unetpp import NestedUNet
+
+from loss import AdaptiveWingLoss
+
+from dataset.gi4e_max import gi4e_eye
+import matplotlib.pyplot as plt
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_path', default=r'/home/snowwhite/eye_tracking/gi4e_database/blend')
+parser.add_argument('--train_path', default=r'/home/snowwhite/eye_tracking/gi4e_database/5fold-new-max/data_train_5.json')
+parser.add_argument('--val_path', default=r'/home/snowwhite/eye_tracking/gi4e_database/5fold-new-max/data_test_5.json')
+parser.add_argument('--test_path', default=r'/home/snowwhite/eye_tracking/gi4e_database/5fold-new-max/data_test_5.json')
+parser.add_argument('--model', default='UNet++') # 可选 [UNet, UNet++]
+parser.add_argument('--loss', default='AdaWing')  # 可选 [AdaWing, MSE]
+parser.add_argument('--img_size', default=64)       # 可选[32，64]
+parser.add_argument('--heatmap_size', default=64)   # 可选[32，64]
+parser.add_argument('--sigma', default=0.5)               # 可选[0.5, 1.0, 2.0]
+parser.add_argument('--pretrained_path', default=None)
+parser.add_argument('--epochs', default=20)
+parser.add_argument('--batch_size', default=8)
+parser.add_argument('--num_workers', default=8)
+parser.add_argument('--save_per_epoch', default=1)
+parser.add_argument('--deepsupervision', default=False)
+parser.add_argument('--randround', default=1)   #可选[0,1]
+args = parser.parse_args()
+
+save_cp_and_plot = True
+save_per_epoch = args.save_per_epoch
+
+workers = args.num_workers
+epochs = args.epochs
+batch_size = args.batch_size
+
+# train settings
+lr = 0.001  
+momentum = 0.9  
+weight_decay = 1e-4  
+lr_decay_milestones = []  
+lr_decay_gamma = 0.1  
+
+
+def main(model):
+    model.cuda()
+    cudnn.benchmark = True 
+    dataTrain = gi4e_eye(data_path=args.data_path, json_path=args.train_path,sigma=float(args.sigma), img_size=int(args.img_size), heatmap_size=int(args.heatmap_size))
+    dataVal = gi4e_eye(data_path=args.data_path, json_path=args.val_path,sigma=float(args.sigma), img_size=int(args.img_size), heatmap_size=int(args.heatmap_size))
+   
+    if args.loss == 'MSE':
+        criterion = nn.MSELoss().cuda()
+    elif args.loss == 'AdaWing':
+        criterion = AdaptiveWingLoss().cuda()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr,
+                                weight_decay=weight_decay)
+    
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, 
+                                               milestones=lr_decay_milestones, 
+                                               gamma=lr_decay_gamma)
+    train(model, criterion, optimizer, scheduler, dataTrain, dataVal)
+
+
+def train(model, criterion, optimizer, scheduler, dataTrain, dataVal):
+    start_time = datetime.now().strftime("%m%d_%H%M")
+    train_loader = torch.utils.data.DataLoader(
+        dataTrain,
+        batch_size=batch_size, shuffle=True,
+        num_workers=workers, pin_memory=True, prefetch_factor=2)
+
+    val_loader = torch.utils.data.DataLoader(
+        dataVal,
+        batch_size=batch_size, shuffle=False,
+        num_workers=workers, pin_memory=True, prefetch_factor=2)
+    print('Sucessfully load trainset:', len(train_loader))
+    print('Sucessfully load valset:', len(val_loader))
+
+    if args.pretrained_path:
+        try:
+            pretrained_dict = torch.load(args.pretrained_path)
+
+            model_dict = model.state_dict()
+
+            pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+
+            model_dict.update(pretrained_dict)
+            model.load_state_dict(model_dict)
+
+            print("Pretrained weights {} loaded successfully.".format(os.path.basename(args.pretrained_path)))
+        except FileNotFoundError:
+            print("Pretrained weights file not found! Training from scratch.")
+        except Exception as e:
+            print("Error loading pretrained weights:", e)
+            print("Training from scratch.")
+
+    global_step = 0
+    e = []
+    train_loss = []
+    val_loss = []
+    train_error = []
+    val_error = []
+    best_val_loss = float('inf')
+    for epoch in range(epochs):
+        model.train()
+        epoch_loss = 0
+        epoch_error = 0
+        with tqdm(total=len(train_loader), desc=f"Epoch {epoch + 1}/{epochs}") as bar:
+            for i, (img, heatmap, eye_pts, pupil_dist, Width, Height, LorR, img_id) in enumerate(train_loader):
+                img = img.cuda()
+                heatmap = heatmap.cuda()
+                eye_pts =eye_pts.cuda()
+                pupil_dist = pupil_dist.cuda()
+                Width = Width.cuda()
+                Height = Height.cuda()
+                pupil_dist = pupil_dist.view(-1, 1)
+                
+                pred_hm = model(img)
+                if args.deepsupervision:
+                    loss = 0
+                    for pred_one_hm in pred_hm:
+                        one_loss = criterion(pred_one_hm, heatmap)
+                        loss += one_loss
+                    pred_hm = pred_hm[3]
+                else:
+                    loss = criterion(pred_hm, heatmap)
+                
+                error, norm_error, all_norm_error = cal_batch_error(pred_hm, eye_pts, Width, Height, pupil_dist, int(args.randround))
+               
+                # compute gradient and do SGD step
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                # increment the total epoch loss
+                epoch_loss += loss.item()
+                epoch_error += norm_error.cpu().item()
+
+                # update the progress bar with the intermediate loss value
+                bar.set_postfix(**{'loss per batch': loss.item()})
+                bar.update()
+
+                global_step += 1
+
+            mean_epoch_loss = round(epoch_loss / len(train_loader), 5)
+            mean_epoch_error = round(epoch_error / len(train_loader), 5)
+            print('\n', f"Epoch {epoch+1} Train Loss:", mean_epoch_loss, "Train Norm Error:", mean_epoch_error)
+            train_loss.append(mean_epoch_loss)
+            train_error.append(mean_epoch_error)
+            e.append(epoch+1)
+
+        scheduler.step()
+    
+        mean_val_loss, mean_val_error = validate(model, val_loader, criterion)
+        print('\n', f"Epoch {epoch+1} Val Loss:", mean_val_loss, "Val Norm Error:", mean_val_error)
+        val_loss.append(mean_val_loss)
+        val_error.append(mean_val_error)
+
+        if save_cp_and_plot:
+            checkpoints_dir = f"results-max/{args.model}_sz{args.img_size}_sig{args.sigma}_ep{args.epochs}_{args.loss}_r{args.randround}"
+            os.makedirs(checkpoints_dir, exist_ok=True)
+            save_loss_jpg(e, train_loss,val_loss,checkpoints_dir)
+            save_error_jpg(e, train_error,val_error,checkpoints_dir)
+            if (epoch+1) % save_per_epoch == 0:
+                cp_name = 'Epoch' + str(epoch + 1) + '.pth'
+                torch.save(model.state_dict(), os.path.join(checkpoints_dir, cp_name))
+                if mean_val_loss < best_val_loss:
+                    best_val_loss = mean_val_loss
+                    torch.save(model.state_dict(), os.path.join(checkpoints_dir, 'best.pth'))
+    mean_test_loss, mean_test_error, mean_inference_time, mean_inference_time_per_frame, error_dict = test_main(model, os.path.join(checkpoints_dir, 'best.pth'))
+    #print(error_dict)
+    output_dict = {
+    'settings': checkpoints_dir,
+    'mean_test_loss': mean_test_loss,
+    'mean_test_norm_error': mean_test_error,
+    'mean_inference_time': mean_inference_time,
+    'mean_inference_time_per_frame': mean_inference_time_per_frame,
+    }
+
+    filename = 'test_result.csv'
+    if not os.path.exists(filename):
+        with open(filename, 'w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=output_dict.keys())
+            writer.writeheader()
+    with open(filename, 'a', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=output_dict.keys())
+        writer.writerow(output_dict)
+    
+
+def validate(model, val_loader, criterion):
+    if len(val_loader) == 0:
+        print("NO VAL SET!")
+        return 0, 0
+    else:
+        model.eval()
+        val_loss=0
+        val_error=0
+        with tqdm(total=len(val_loader), desc="Validation") as vbar:
+            for i, (img, heatmap, eye_pts, pupil_dist, Width, Height, LorR, img_id) in enumerate(val_loader):
+                img = img.cuda()
+                heatmap = heatmap.cuda()
+                eye_pts =eye_pts.cuda()
+                pupil_dist = pupil_dist.cuda()
+                Width = Width.cuda()
+                Height = Height.cuda()
+                pupil_dist = pupil_dist.view(-1, 1)
+                pred_hm = model(img)
+                if args.deepsupervision:
+                    loss = 0
+                    for pred_one_hm in pred_hm:
+                        one_loss = criterion(pred_one_hm, heatmap)
+                        loss += one_loss
+                    pred_hm = pred_hm[3]
+                else:
+                    loss = criterion(pred_hm, heatmap)
+                error, norm_error, all_norm_error = cal_batch_error(pred_hm, eye_pts, Width, Height, pupil_dist, int(args.randround))
+
+                val_loss += loss.item()
+                val_error += norm_error.cpu().item()
+
+
+                vbar.set_postfix(**{'loss per batch': loss.item()})
+                vbar.update()
+            mean_val_loss = round(val_loss / len(val_loader), 5)
+            mean_val_error = round(val_error / len(val_loader), 5)
+        return mean_val_loss, mean_val_error
+
+
+def save_checkpoint(model, epoch, save_per_epoch, checkpoints_dir, best_val_loss, mean_val_loss):
+    if (epoch+1) % save_per_epoch == 0:
+        cp_name = 'Epoch' + str(epoch + 1) + '.pth'
+        torch.save(model.state_dict(), os.path.join(checkpoints_dir, cp_name))
+        if mean_val_loss < best_val_loss:
+            best_val_loss = best_val_loss
+            torch.save(model.state_dict(), os.path.join(checkpoints_dir, 'Best.pth'))
+
+def save_loss_jpg(e, train_loss, val_loss, checkpoints_dir):
+    plt.figure(figsize=(10,8))
+    plt.plot(e, train_loss, linestyle="-", color="red", marker="o", label="train_loss")
+    plt.plot(e, val_loss, linestyle="-", color="green", marker="o", label="val_loss")
+    plt.xlabel("Epochs", fontsize=16)
+    plt.ylabel("Loss", fontsize=16)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.legend(fontsize=16, markerscale=2)
+    plt.tight_layout()
+    plt.savefig(os.path.join(checkpoints_dir, "loss_plot.jpg"), dpi=600)
+
+def save_error_jpg(e, train_loss, val_loss, checkpoints_dir):
+    plt.figure(figsize=(10,8))
+    plt.plot(e, train_loss, linestyle="-", color="red", marker="o", label="train_error")
+    plt.plot(e, val_loss, linestyle="-", color="green", marker="o", label="val_error")
+    plt.xlabel("Epochs", fontsize=16)
+    plt.ylabel("Error", fontsize=16)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.legend(fontsize=16, markerscale=2)
+    plt.tight_layout()
+    plt.savefig(os.path.join(checkpoints_dir, "error_plot.jpg"), dpi=600)
+
+
+def test_main(model, weight_path):
+    model.eval()
+    model = load_model(model, weight_path)
+    model = model.cuda()
+    dataTest = gi4e_eye(data_path=args.data_path, json_path=args.test_path, sigma=float(args.sigma), img_size=int(args.img_size), heatmap_size=int(args.heatmap_size))
+
+    if args.loss == 'MSE':
+        criterion = nn.MSELoss().cuda()
+    elif args.loss == 'AdaWing':
+        criterion = AdaptiveWingLoss().cuda()
+
+    test_loader = torch.utils.data.DataLoader(
+        dataTest,
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.num_workers, pin_memory=True, prefetch_factor=2)
+    mean_test_loss, mean_test_error, mean_inference_time, mean_inference_time_per_frame, error_dict = test(model, test_loader, criterion)
+    return mean_test_loss, mean_test_error, mean_inference_time, mean_inference_time_per_frame, error_dict
+
+def test(model, test_loader, criterion):
+    if len(test_loader) == 0:
+        print("NO Test SET!")
+        return 0, 0
+    else:
+        test_loss=0
+        test_error=0
+        total_images = 0
+        inference_times = []
+        error_dict = {}
+        with tqdm(total=len(test_loader), desc="Test") as vbar:
+            for i, (img, heatmap, eye_pts, pupil_dist, Width, Height, LorR, img_id) in enumerate(test_loader):
+                start_time = time.time() 
+                img = img.cuda()
+                heatmap = heatmap.cuda()
+                eye_pts =eye_pts.cuda()
+                pupil_dist = pupil_dist.cuda()
+                Width = Width.cuda()
+                Height = Height.cuda()
+                pupil_dist = pupil_dist.view(-1, 1)
+
+                pred_hm = model(img)
+                if args.deepsupervision:
+                    loss = 0
+                    for pred_one_hm in pred_hm:
+                        one_loss = criterion(pred_one_hm, heatmap)
+                        loss += one_loss
+                    pred_hm = pred_hm[3]   
+                else:
+                    loss = criterion(pred_hm, heatmap)
+                
+                error, norm_error, all_norm_error = cal_batch_error(pred_hm, eye_pts, Width, Height, pupil_dist, int(args.randround))
+
+                test_loss += loss.item()
+                test_error += norm_error.cpu().item()
+                end_time = time.time() 
+                inference_times.append(end_time - start_time) 
+                for i, batch_errors in enumerate(all_norm_error.detach().cpu().numpy()):
+                    #for e in batch_errors:
+                    e = batch_errors[1]
+                    current_id = img_id[i]
+                    current_LorR = LorR[i]
+                    if current_id not in error_dict:
+                        error_dict[current_id] = []
+                    error_dict[current_id].append(e)
+                    total_images += 1
+                vbar.set_postfix(**{'loss per batch': loss.item()})
+                vbar.update()
+            error_rate_0025, error_rate_005, error_rate_001 = cal_error_max(error_dict)
+            mean_test_loss = round(test_loss / len(test_loader), 5)
+            mean_test_error = round(test_error / len(test_loader), 5)
+            mean_inference_time = round(sum(inference_times) / len(inference_times), 5)
+            mean_inference_time_per_frame = round(mean_inference_time/int(args.batch_size), 5)
+        print("Error < 0.025:", error_rate_0025, "Error < 0.05:", error_rate_005, "Error < 0.01:", error_rate_001)
+        print("Test Loss: ", mean_test_loss, "Test Norm Error", mean_test_error, "Inference Time(s) Per Batch({}):".format(args.batch_size), mean_inference_time, "Mean Inference Time(s) Per Frame:", mean_inference_time_per_frame)
+        
+        return mean_test_loss, mean_test_error, mean_inference_time, mean_inference_time_per_frame, error_dict
+
+
+def load_model(model, weight_path):
+    if weight_path is None:
+        print("No weights path provided!")
+        sys.exit(1)
+
+    try:
+        pretrained_dict = torch.load(weight_path)
+        model_dict = model.state_dict()
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+
+        print("Weights {} loaded successfully.".format(os.path.basename(weight_path)))
+    except FileNotFoundError:
+        print("Weights file not found!")
+        sys.exit(1)
+    except Exception as e:
+        print("Error loading pretrained weights:", e)
+        sys.exit(1)
+    return model
+
+def cal_error_max(error_dict):
+    error_count_0025 = 0
+    error_count_005 = 0
+    error_count_001 = 0
+    total_images = len(error_dict)
+    for value in error_dict.values():
+        e = max(value)
+        if e < 0.025:
+            error_count_0025 += 1
+        if e < 0.05:
+            error_count_005 += 1
+        if e < 0.1:
+            error_count_001 += 1
+    error_rate_0025 = error_count_0025 / total_images
+    error_rate_005 = error_count_005 / total_images
+    error_rate_001 = error_count_001 / total_images
+    
+    return error_rate_0025, error_rate_005, error_rate_001
+    
+
+if __name__ == "__main__":
+    if args.model == 'UNet':
+        model = UNet()
+    elif args.model == 'UNet++':
+        model = NestedUNet(args.deepsupervision)
+    main(model)
